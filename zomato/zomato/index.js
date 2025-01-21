740ccf43aaf6ac5ca7d039e5f471358a1b00619b
Tanaka Godzi <godzit@africau.edu>
2025-01-21 09:16:51 +0200

    Added Serice Worker registration code  to index.js

--- zomato/zomato/index.js
+++ zomato/zomato/index.js
@@ -1,3 +1,16 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+      navigator.serviceWorker.register('sw.js')
+          .then((registration) => {
+              console.log('Service Worker registered with scope:', registration.scope);
+          })
+          .catch((error) => {
+              console.error('Service Worker registration failed:', error);
+          });
+  });
+}
+
+
 // IndexedDB setup
 const dbPromise = idb.openDB("zomato-db", 1, {
     upgrade(db) {
