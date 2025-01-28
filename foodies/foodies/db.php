ff63352c9789848b8c35cc7c3b4b5c14540fea9b
Tadiwa02 <chinyadzat@africau.edu>
2025-01-28 11:15:31 +0200

    worked on db.php

--- foodies/foodies/db.php
+++ foodies/foodies/db.php
@@ -2,7 +2,7 @@
 $servername = "localhost"; // Your database server
 $username = "root"; // Your database username
 $password = ""; // Your database password
-$dbname = "zomato"; // Your database name
+$dbname = "zomato"; // Your database name here 
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
